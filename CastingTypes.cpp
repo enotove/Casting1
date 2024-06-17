ecfc1c80c13d315fcaf2b22116098e57b0d22de4
Enotove <enotove@gmail.com>
2024-06-17 13:51:25 +0500

    Fixed solution

--- CastingTypes.cpp
+++ CastingTypes.cpp
@@ -15,7 +15,14 @@ public:
 		denominator_ = denominator;
 	}
 	
-	bool operator ==(Fraction other) { return ((numerator_ == other.numerator_) && (denominator_ == other.denominator_)); }
+	bool operator ==(Fraction other) {
+
+
+		int temp_numerator1 = numerator_ * other.denominator_;
+		int temp_numerator2 = other.numerator_ * denominator_;
+		if (temp_numerator1 == temp_numerator2) return 1;
+		else return 0;
+	}
 	bool operator !=(Fraction other) { return !(*this == other); }
 	bool operator >(Fraction other) {
 		int temp_numerator1 = numerator_ * other.denominator_;
@@ -31,7 +38,7 @@ public:
 int main()
 {
 	Fraction f1(4, 3);
-	Fraction f2(6, 11);
+	Fraction f2(8, 6);
 
 	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
 	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
